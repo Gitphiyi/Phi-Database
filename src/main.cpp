e004ba9ac0b38e120ae3e6acfdef4f01875d718a
Philip Yi <philip.yunfan.yi@gmail.com>
2025-08-03 12:08:02 -0500

    cmake stuf

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include "Page.hpp"
 #include "DbFile.hpp"
+#include <sys/stat.h>
 
 using namespace DB;
 // bazel run --config=local_run src:main
 
 int main(int argc, char** argv) {
+    setup();
     const char* filename = "hi.db";
     std::cout << "Creating dbfile at: " << filename << std::endl;
     Page testPage{Page(-1)};
     DbFile dbFile = DbFile(filename, true, testPage);
     return 1;
+}
+
+void setup() {
+    mkdir("db", 0755);
 }
